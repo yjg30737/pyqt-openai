c908b592690b58b219c368ded3416ecdfbbea1b8
Jung Gyu Yoon <yjg30737@gmail.com>
2023-02-12 14:58:01 +0900

    make user able to select each property including model(engine), prevent input the text while AI generating the response

--- pyqt_openai/main.py
+++ pyqt_openai/main.py
@@ -3,11 +3,12 @@ import openai
 from chatWidget import Prompt, ChatBrowser
 
 # this API key should be yours
-# openai.api_key = '[MY_OPENAPI_API_KEY]'
+openai.api_key = '[MY_OPENAPI_API_KEY]'
 
 from PyQt5.QtCore import Qt, QCoreApplication, QThread, pyqtSignal
 from PyQt5.QtGui import QGuiApplication, QFont
-from PyQt5.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QWidget, QSplitter, QComboBox, QSpinBox, \
+    QFormLayout, QDoubleSpinBox
 
 QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
 QCoreApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)  # HighDPI support
@@ -19,20 +20,16 @@ QApplication.setFont(QFont('Arial', 12))
 class OpenAIThread(QThread):
     replyGenerated = pyqtSignal(str, bool)
 
-    def __init__(self, question, *args, **kwargs):
+    def __init__(self, openai_arg, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__question = question
+        self.__openai_arg = openai_arg
 
     def run(self):
+
         openai_object = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=self.__question,
-            temperature=0.5,
-            max_tokens=256,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0
+            **self.__openai_arg
         )
+
         response_text = openai_object['choices'][0]['text'].strip()
 
         self.replyGenerated.emit(response_text, False)
@@ -41,10 +38,19 @@ class OpenAIThread(QThread):
 class OpenAIChatBot(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.__initVal()
         self.__initUi()
 
+    def __initVal(self):
+        self.__engine = "text-davinci-003"
+        self.__temperature = 0.0
+        self.__max_tokens = 256
+        self.__top_p = 1.0
+        self.__frequency_penalty = 0.0
+        self.__presence_penalty = 0.0
+
     def __initUi(self):
-        self.setWindowTitle('PyQt OpenAI Chatbot v0.0.1')
+        self.setWindowTitle('PyQt OpenAI Chatbot')
         self.__prompt = Prompt()
         self.__lineEdit = self.__prompt.getTextEdit()
         self.__lineEdit.setPlaceholderText('Write some text...')
@@ -54,20 +60,121 @@ class OpenAIChatBot(QMainWindow):
         lay.addWidget(self.__browser)
         lay.addWidget(self.__prompt)
         lay.setSpacing(0)
-        mainWidget = QWidget()
-        mainWidget.setLayout(lay)
+        chatWidget = QWidget()
+        chatWidget.setLayout(lay)
+
+        modelComboBox = QComboBox()
+        modelComboBox.addItems([
+            'text-davinci-003',
+            'text-curie-001',
+            'text-babbage-001',
+            'text-ada-001',
+            'code-davinci-002',
+            'code-cushman-001'
+        ])
+        modelComboBox.setCurrentText(self.__engine)
+        modelComboBox.currentTextChanged.connect(self.__modelChanged)
+
+        temperatureSpinBox = QDoubleSpinBox()
+        temperatureSpinBox.setRange(0, 1)
+        temperatureSpinBox.setAccelerated(True)
+        temperatureSpinBox.setSingleStep(0.01)
+        temperatureSpinBox.setValue(self.__temperature)
+        temperatureSpinBox.valueChanged.connect(self.__temperatureChanged)
+
+        maxTokensSpinBox = QSpinBox()
+        maxTokensSpinBox.setRange(0, 4000)
+        maxTokensSpinBox.setAccelerated(True)
+        maxTokensSpinBox.setValue(self.__max_tokens)
+        maxTokensSpinBox.valueChanged.connect(self.__maxTokensChanged)
+
+        toppSpinBox = QDoubleSpinBox()
+        toppSpinBox.setRange(0, 1)
+        toppSpinBox.setAccelerated(True)
+        toppSpinBox.setSingleStep(0.01)
+        toppSpinBox.setValue(self.__top_p)
+        toppSpinBox.valueChanged.connect(self.__toppChanged)
+
+        frequencyPenaltySpinBox = QDoubleSpinBox()
+        frequencyPenaltySpinBox.setRange(0, 2)
+        frequencyPenaltySpinBox.setAccelerated(True)
+        frequencyPenaltySpinBox.setSingleStep(0.01)
+        frequencyPenaltySpinBox.setValue(self.__frequency_penalty)
+        frequencyPenaltySpinBox.valueChanged.connect(self.__frequencyPenaltyChanged)
+
+        presencePenaltySpinBox = QDoubleSpinBox()
+        presencePenaltySpinBox.setRange(0, 2)
+        presencePenaltySpinBox.setAccelerated(True)
+        presencePenaltySpinBox.setSingleStep(0.01)
+        presencePenaltySpinBox.setValue(self.__presence_penalty)
+        presencePenaltySpinBox.valueChanged.connect(self.__presencePenaltyChanged)
+
+        lay = QFormLayout()
+        lay.addRow('Option', None)
+        lay.addRow('Model', modelComboBox)
+        lay.addRow('Temperature', temperatureSpinBox)
+        lay.addRow('Maximum length', maxTokensSpinBox)
+        lay.addRow('Top P', toppSpinBox)
+        lay.addRow('Frequency penalty', frequencyPenaltySpinBox)
+        lay.addRow('Presence penalty', presencePenaltySpinBox)
+
+        sidebarWidget = QWidget()
+        sidebarWidget.setLayout(lay)
+
+        mainWidget = QSplitter()
+        mainWidget.addWidget(chatWidget)
+        mainWidget.addWidget(sidebarWidget)
+        mainWidget.setSizes([700, 300])
+        mainWidget.setChildrenCollapsible(False)
+        mainWidget.setHandleWidth(2)
         self.setCentralWidget(mainWidget)
-        self.resize(600, 400)
+        self.resize(1024, 768)
 
         self.__browser.showText('Hello!', True)
         self.__browser.showText('Hello! How may i help you?', False)
 
+        self.__lineEdit.setFocus()
+
     def __chat(self):
-        self.__t = OpenAIThread(self.__lineEdit.toPlainText())
+        openai_arg = {
+            'engine': self.__engine,
+            'prompt': self.__lineEdit.toPlainText(),
+            'temperature': self.__temperature,
+            'max_tokens': self.__max_tokens,
+            'top_p': self.__top_p,
+            'frequency_penalty': self.__frequency_penalty,
+            'presence_penalty': self.__presence_penalty,
+        }
+
+        self.__lineEdit.setEnabled(False)
+        self.__t = OpenAIThread(openai_arg)
         self.__t.replyGenerated.connect(self.__browser.showText)
         self.__browser.showText(self.__lineEdit.toPlainText(), True)
         self.__lineEdit.clear()
         self.__t.start()
+        self.__t.finished.connect(self.__afterGenerated)
+
+    def __afterGenerated(self):
+        self.__lineEdit.setEnabled(True)
+        self.__lineEdit.setFocus()
+
+    def __modelChanged(self, v):
+        self.__engine = v
+
+    def __temperatureChanged(self, v):
+        self.__temperature = round(v, 2)
+
+    def __maxTokensChanged(self, v):
+        self.__max_tokens = round(v, 2)
+
+    def __toppChanged(self, v):
+        self.__topp = round(v, 2)
+
+    def __frequencyPenaltyChanged(self, v):
+        self.__frequency_penalty = round(v, 2)
+
+    def __presencePenaltyChanged(self, v):
+        self.__presence_penalty = round(v, 2)
 
 
 if __name__ == "__main__":
