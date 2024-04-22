0b08d6c72dbccbb72c101a9c8dd691eef4ec2490
Jung Gyu Yoon <yjg30737@gmail.com>
2024-04-22 11:50:17 +0900

    Bump ver

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 
 setup(
     name='pyqt-openai',
-    version='0.2.63',
+    version='0.3.0',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
